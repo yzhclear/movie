e4fe9dc4a2805e038810218c053557261c7f2ff6
yzhclear <188225829@qq.com>
2018-11-09 15:28:17 +0800

    增加puppeteer爬虫爬取数据

---
+++ server/crawler/trailer-list.js
@@ -0,0 +1,61 @@
+const puppeteer = require('puppeteer')
+const { resolve } = require('path')
+
+const url = 'https://movie.douban.com/tag/#/?sort=U&range=6,10&tags='
+const chromeDir = resolve(__dirname, '../../node_modules/chrome/Chromium.app/Contents/MacOS/Chromium')
+
+const sleep = time => new Promise(resolve => {
+    setTimeout(resolve, time)
+})
+
+;(async () => {
+    console.log('start visit the target page')
+
+    const browser = await puppeteer.launch({
+        executablePath: chromeDir,
+        args: ['--no-sanbox'],
+        dumpio: false
+    })
+
+    const page = await browser.newPage()
+    await page.goto(url, {
+        waitUntil: 'networkidle2'
+    })
+
+    await sleep(3000)
+
+    await page.waitForSelector('.more')
+
+    for (let i = 0; i < 1; i ++) { //only get another one page data
+        await sleep(3000)
+        await page.click('.more')
+    }
+
+    const result = await page.evaluate(() => {
+        var $ = window.$
+        var items = $('.list-wp a')
+        var links = []
+
+        if (items.length >= 1) {
+            items.each((index, item) => {
+                let it = $(item)
+                let doubanId = it.find('div').data('id')
+                let title = it.find('.title').text()
+                let rate = Number(it.find('.rate').text())
+                let poster = it.find('img').attr('src').replace('s_ratio', 'l_ratio')
+
+                links.push({
+                    doubanId,
+                    title,
+                    rate,
+                    poster
+                })
+
+            })
+        }
+        return links
+    })
+
+    browser.close()
+    console.log(result)
+})()
